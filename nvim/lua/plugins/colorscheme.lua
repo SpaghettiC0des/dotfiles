88e01c2d36b9311ff307fa90b696b008fa3551d4
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-05-23 00:27:59 +0800

    fix(nvim): darker tokyonight bg

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,76 @@ return {
   -- {
   --   "oxfist/night-owl.nvim",
   -- },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     local palletes = {
+  --       all = { red = "#ff0000" },
+  --       github_dark_default = {
+  --         -- Defining multiple shades is done by passing a table
+  --         red = {
+  --           base = "#8e1519",
+  --           bright = "#ee0000",
+  --         },
+  --       },
+  --     }
+  --     require("github-theme").setup({
+  --       palletes = palletes,
+  --     })
+  --   end,
+  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        local prompt = "#29202b"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+      on_colors = function(colors)
+        colors.bg = "#0d0414"
+        -- colors.hint = colors.orange
+        -- colors.error = "#ff0000"
+      end,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "night-owl",
       colorscheme = "tokyonight-night",
       -- colorscheme = "catppuccin",
+      -- colorscheme = "github_dark_default",
     },
   },
 }
