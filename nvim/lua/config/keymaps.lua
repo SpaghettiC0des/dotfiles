4d92207f03f0009f9f8391bc7c31d598476dba37
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-11-19 05:08:51 +0800

    fix(nvim): map `kj` to `esc`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 map("i", "jk", "<esc>", { noremap = true, silent = true })
+map("i", "kj", "<esc>", { noremap = true, silent = true })
 
 -- Remove default move Lines
 vim.keymap.del({ "n", "v", "i" }, "<M-j>")
