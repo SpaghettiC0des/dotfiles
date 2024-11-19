cc12cfbe312b9870faa594a05d09b603652b306f
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-11-20 01:31:55 +0800

    fix(lazyvim): try a 2 pane dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,27 +7,39 @@ return {
       dashboard = {
         preset = {
           header = [[
-  ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
-  ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
-  ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗
-  ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝
-   ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
-    ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝
-        ]],
+  ██╗  ██╗ █████╗ ██████╗ ██╗         ██╗   ██╗██╗███╗   ███╗
+  ██║ ██╔╝██╔══██╗██╔══██╗██║         ██║   ██║██║████╗ ████║
+  █████╔╝ ███████║██████╔╝██║         ██║   ██║██║██╔████╔██║
+  ██╔═██╗ ██╔══██║██╔══██╗██║         ╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║  ██╗██║  ██║██║  ██║███████╗     ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝      ╚═══╝  ╚═╝╚═╝     ╚═╝
+]],
         },
         sections = {
-          { section = "header", pane = 2 },
-          {
-            section = "terminal",
-            cmd = "chafa ~/.config/nvim/dashboard.jpg --colors full --dither ordered --dither-intensity 1.0 --format symbols --symbols all --size 60x27 --stretch; sleep .1",
-            height = 27,
-            padding = 1,
-          },
+          { section = "header" },
           {
             pane = 2,
-            { section = "keys", gap = 1, padding = 1 },
-            { section = "startup" },
+            section = "terminal",
+            cmd = "chafa ~/.config/nvim/dashboard.jpg --format symbols --symbols vhalf --size 60x100; sleep .1",
+            height = 15,
+            padding = 1,
           },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          -- {
+          --   pane = 2,
+          --   icon = " ",
+          --   title = "Git Status",
+          --   section = "terminal",
+          --   enabled = vim.fn.isdirectory(".git") == 1,
+          --   cmd = "hub status --short --branch --renames",
+          --   height = 5,
+          --   padding = 1,
+          --   ttl = 5 * 60,
+          --   indent = 3,
+          -- },
+          { section = "startup" },
         },
       },
     },
