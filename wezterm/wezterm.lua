5dbd7a21a8939d48b66b0a066683c81833265a09
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-11-20 16:14:26 +0800

    fix(wezterm): initial workspace automation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local session_manager = require("wezterm-session-manager/session-manager")
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local act = wezterm.action
 -- This table will hold the configuration.
 local config = {}
@@ -57,11 +58,24 @@ wezterm.on("restore_session", function(window)
 	session_manager.restore_state(window)
 end)
 -- This is where you actually apply your config choices
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	local editorTab = tab
+	window:gui_window():maximize()
+	tab:set_title("editor")
+	pane:split({ size = 0.3, direction = "Bottom" })
+	---@diagnostic disable-next-line: redefined-local
+	local tab, pane, window = window:spawn_tab({})
+	tab:set_title("terminal")
+	pane:split({ size = 0.5, direction = "Bottom" })
+	editorTab:activate()
+	editorTab:panes()[1]:activate()
+end)
 
 config.max_fps = 120
 config.window_background_opacity = 0.5
 config.macos_window_background_blur = 80
-config.font_size = 30.0
+config.font_size = 25.0
 -- config.font = wezterm.font("Operator Mono Lig")
 config.font = wezterm.font("BlexMono Nerd Font")
 -- config.font = wezterm.font("Ligalex Mono")
