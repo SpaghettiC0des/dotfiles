390d0829a7dce59dbdadea9dd671791a8ca8c0f2
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-12-06 02:16:43 +0800

    chore(lazyvim): disable smear-cursor for now

--- nvim/lua/plugins/playground.lua
+++ nvim/lua/plugins/playground.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    "sphamba/smear-cursor.nvim",
-    opts = {},
-  },
+  -- {
+  --   "sphamba/smear-cursor.nvim",
+  --   opts = {},
+  -- },
   -- {
   --   "tris203/precognition.nvim",
   --   --event = "VeryLazy",
