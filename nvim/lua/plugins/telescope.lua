1c2d6c5dd60d363041db5b19d3e6393d8384853b
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-12-11 04:15:02 +0800

    fix(lazyvim): remove unnecessary telescope dependency(fzf), provided by default

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,53 +10,12 @@ return {
   -- },
   {
     "telescope.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        event = "VeryLazy",
-        config = function(_, _)
-          require("lazyvim.util").on_load("telescope.nvim", function()
-            require("telescope").load_extension("live_grep_args")
-          end)
-        end,
-        keys = {
-          { "<leader>/", ":Telescope live_grep_args<CR>", desc = "Live Grep Args" },
-        },
-      },
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = vim.fn.executable("make") == 1 and "make"
-          or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-        enabled = vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1,
-        config = function()
-          LazyVim.on_load("telescope.nvim", function()
-            local t = require("telescope")
-            t.load_extension("fzf")
-
-            t.load_extension("flutter")
-            -- t.load_extension("media_files")
-            t.load_extension("package_info")
-            t.setup({
-              pickers = { find_files = { hidden = true } },
-              -- find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
-              extensions = {
-                package_info = {
-                  -- Optional theme (the extension doesn't set a default theme)
-                  theme = "ivy",
-                },
-              },
-            })
-            -- require("telescope").load_extension("live_grep_args")
-          end)
-        end,
-      },
-    },
     keys = {
-      {
-        "<leader>/",
-        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-        desc = "Grep (root dir)",
-      },
+      -- {
+      --   "<leader>/",
+      --   "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      --   desc = "Grep (root dir)",
+      -- },
       {
         "<leader>s-",
         "<cmd>:Telescope grep_string<CR>",
@@ -70,10 +29,24 @@ return {
         desc = "Find Plugin File",
       },
     },
-    config = function(_, opts)
-      local t = require("telescope")
-      t.setup(opts)
-      t.load_extension("live_grep_args")
+    config = function()
+      LazyVim.on_load("telescope.nvim", function()
+        local t = require("telescope")
+
+        t.load_extension("flutter")
+        -- t.load_extension("media_files")
+        t.load_extension("package_info")
+        t.setup({
+          pickers = { find_files = { hidden = true } },
+          extensions = {
+            package_info = {
+              -- Optional theme (the extension doesn't set a default theme)
+              theme = "ivy",
+            },
+          },
+        })
+        -- require("telescope").load_extension("live_grep_args")
+      end)
     end,
   },
 }
