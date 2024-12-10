b94b89cd520411bfe67fdabd0badb996e0fed307
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-12-10 16:35:12 +0800

    fix(lazyvim): enable flutter color highlighting

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -1,25 +1,41 @@
 -- requires dart/flutter to be installed
 return {
-  recommended = {
-    ft = "dart",
-    root = { "pubspec.yaml" },
-  },
+  -- recommended = {
+  --   ft = "dart",
+  --   root = { "pubspec.yaml" },
+  -- },
   {
     "akinsho/flutter-tools.nvim",
-    ft = "dart",
+    -- ft = "dart",
+    -- lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim",
     },
-    config = true,
+    config = function()
+      require("flutter-tools").setup({
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = true, --
+          },
+        },
+        debugger = {
+          enabled = true,
+          register_configurations = function(_)
+            require("dap").configurations.dart = {}
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+      })
+    end,
   },
 
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "dart" } },
   },
-  -- {
-  --   "nvimtools/none-ls.nvim",
+  -- { "nvimtools/none-ls.nvim",
   --   optional = true,
   --   opts = function(_, opts)
   --     local nls = require("null-ls")
