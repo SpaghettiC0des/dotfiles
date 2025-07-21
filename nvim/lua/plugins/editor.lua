e42ff8a6a9089acd9f2b8946ea5937dd625af1b6
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2025-07-22 03:20:16 +0800

    fix(nvim): disable fzf-lua

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "ibhagwan/fzf-lua",
+    enabled = false,
+  },
+  {
     "mrjones2014/smart-splits.nvim",
     lazy = true,
     keys = {
--- nvim/lua/plugins/fzf.lua
+++
@@ -1,6 +0,0 @@
--- Disabled in favor of snacks.nvim picker
-return {
-  "ibhagwan/fzf-lua",
-  enabled = false,
-}
-
